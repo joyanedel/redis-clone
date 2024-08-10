ea30b98c2ecda4111cbea1008756178f939f2e2a
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-08-10 18:09:30 -0400

    implement ping response

--- src/main.rs
+++ src/main.rs
@@ -21,26 +21,35 @@ async fn main() -> io::Result<()> {
 async fn accept_connection(conn: TcpStream) -> io::Result<()> {
     loop {
         let mut buf = [0; 512];
-        match conn.try_read(&mut buf) {
+        let command = match conn.try_read(&mut buf) {
             Ok(0) => break,
             Ok(_) => {
                 let command = String::from_utf8_lossy(&buf).to_string();
-                parse_command(command);
-                conn.try_write("+PONG\r\n".as_bytes())
+                let parsed_command = parse_command(command);
+                parsed_command
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
             Err(e) => return Err(e),
-        }
-        .unwrap();
+        };
+
+        responds_to_client(command, &conn).expect("couldn't respond to client");
     }
 
     Ok(())
 }
 
-fn parse_command(command: String) {
+fn parse_command(command: String) -> RedisCommand {
     let client_input = RESPValues::try_from(command.as_str()).expect("couldn't parse client input");
     let command =
         RedisCommand::try_from(client_input.clone()).expect("couldn't parse client command");
-    println!("Input: {client_input:?}");
-    println!("Command: {command:?}");
+    command
+}
+
+fn responds_to_client(command: RedisCommand, conn: &TcpStream) -> io::Result<usize> {
+    match command {
+        RedisCommand::Ping(Some(v)) => conn.try_write(format!("+\"{v}\"\r\n").as_bytes()),
+        RedisCommand::Ping(_) => conn.try_write("+PONG\r\n".as_bytes()),
+        _ => unimplemented!(),
+    }
+    // conn.try_write("+PONG\r\n".as_bytes())
 }
